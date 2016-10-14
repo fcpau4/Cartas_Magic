a863fa6fc1cb472cada719b86ed44f1bf129a743
fcpau <fcpau4@gmail.com>
2016-10-14 16:55:07 +0200

    Interconnectem el layout lista_cartas_row amb el fragment.

--- app/src/main/java/com/example/a47276138y/cartas_magic/Cartas_MagicFragment.java
+++ app/src/main/java/com/example/a47276138y/cartas_magic/Cartas_MagicFragment.java
@@ -5,14 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class Cartas_MagicFragment extends Fragment {
 
-
+    private ArrayAdapter<String> adapter;
 
     public Cartas_MagicFragment() {
     }
@@ -24,6 +28,18 @@ public class Cartas_MagicFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_cartas__magic, container, false);
 
         ListView listaCartas = (ListView) view.findViewById(R.id.listaCartas);
+
+        String[] data = {"carta1", "carta2", "carta3", "carta4"};
+        ArrayList<String> dataList = new ArrayList<>(Arrays.asList(data));
+
+        adapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.lista_cartas_row,
+                R.id.carta,
+                dataList
+        );
+
+        listaCartas.setAdapter(adapter);
 
         return view;
     }
