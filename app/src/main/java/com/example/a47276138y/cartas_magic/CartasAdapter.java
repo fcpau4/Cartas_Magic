3739fd4847d91fe33b128688ac5b269c75b58401
fcpau <fcpau4@gmail.com>
2016-10-21 16:48:57 +0200

    Definim el getView perquè se'ns mostrin les dades com vulguem.

--- app/src/main/java/com/example/a47276138y/cartas_magic/CartasAdapter.java
+++ app/src/main/java/com/example/a47276138y/cartas_magic/CartasAdapter.java
@@ -1,7 +1,13 @@
 package com.example.a47276138y.cartas_magic;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -13,5 +19,26 @@ public class CartasAdapter extends ArrayAdapter<Carta> {
 
     public CartasAdapter(Context context, int resource, List<Carta> objects){
         super(context, resource, objects);
+    }
+
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Carta carta = getItem(position);
+        Log.w("XXXX", carta.toString());
+
+        if(convertView==null){
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.lista_cartas_row, parent, false);
+        }
+
+        TextView name_carta = (TextView) convertView.findViewById(R.id.name);
+        TextView carta_tipos = (TextView) convertView.findViewById(R.id.types);
+
+        name_carta.setText(carta.getName());
+        carta_tipos.setText(carta.getTipos());
+
+        return convertView;
     }
 }
